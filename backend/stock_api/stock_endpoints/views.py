62212a5e6b144c82b4680e81866385ef58ab5bb4
EliasPinno <pucas946@gmail.com>
2023-10-04 10:36:33 -0600

    Added some small docstrings.

--- backend/stock_api/stock_endpoints/views.py
+++ backend/stock_api/stock_endpoints/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-# Create your views here.
 @api_view(["GET"])
 def getAllDataForTicker(request, ticker: str):
+    """
+    Returns a response with all of the data stored for a given ticker.
+    """
     return Response({"ticker": ticker})
 
 @api_view(["POST"])
 def getDataForSingleDay(request):
+    """
+    Returns all of the data for the given tickers for the given day.
+    """
     return Response(request)
 
 @api_view(["POST"])
 def getDataForDateRange(request):
+    """
+    Returns all of the data for the given tickers for the given date range.
+    """
     return Response({"diff": "ticker"})
