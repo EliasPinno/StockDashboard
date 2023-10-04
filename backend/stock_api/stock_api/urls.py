f6d7671ee8028e6dc85e4e978c8ced08490da5bb
EliasPinno <pucas946@gmail.com>
2023-10-04 10:10:59 -0600

    Added api to endpoint routes.

--- backend/stock_api/stock_api/urls.py
+++ backend/stock_api/stock_api/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("stock_endpoints.urls"))
+    path("api/", include("stock_endpoints.urls"))
 ]
