bd790c66a4178525536e463a037a8d2ee1b79a8f
EliasPinno <pucas946@gmail.com>
2023-10-04 15:55:54 -0600

    Added function to update DB to all endpoints.

--- backend/stock_api/stock_endpoints/views.py
+++ backend/stock_api/stock_endpoints/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .data_utils.AlphaVantage import getAlphaVantageAPIInstance
 from .data_utils.DatabaseCommands import getDBInstance
+from typing import List
 
 @api_view(["GET"])
 def getAllDataForTicker(request, ticker: str):
@@ -10,41 +11,29 @@ def getAllDataForTicker(request, ticker: str):
     Returns a response with all of the data stored for a given ticker.
     """
     if len(ticker) > 5:
-        return Response({"result":"error"})
+        return Response({"error":"Ticker is too long."}, status=400)
+    updateTickerList([ticker])
     db = getDBInstance()
-    #db.dropTable()
-    #db.createDatabase()
-    cachedTicker = db.getMostRecentDateForTickers([ticker])
-    api = getAlphaVantageAPIInstance()
-    apiResponse = api.getTickerData(ticker)
-    if ticker not in cachedTicker:
-        print("Ticker not in db: cache from scratch.")
-        dataToCache = apiResponse["Time Series (Daily)"]
-        dbFormOfData = []
-        for date in dataToCache.keys():
-            print(date)
-            dbFormOfData.append((ticker, date, dataToCache[date]["4. close"]))
-        print(dbFormOfData)
-        db.insertData(dbFormOfData)
-    elif cachedTicker[ticker] != apiResponse["Meta Data"]["3. Last Refreshed"]:
-        print("Ticker in db but not updated: compute diff")
-        pass
     print("Data is now cached, returning cache.")
-    return Response({"result":db.getAllDataForTicker(ticker)})
+    result = db.getAllDataForTicker(ticker)
+    db.closeConnection()
+    return Response({"result": result})
 
 @api_view(["POST"])
 def getDataForSingleDay(request):
+    """
+        Returns all of the data for the given tickers for the given day.
+    """
     try:
         request_data = json.loads(request.body.decode("utf-8"))
     except json.JSONDecodeError:
         return Response({"error": "Invalid JSON data"}, status=400)
-    """
-    Returns all of the data for the given tickers for the given day.
-    """
     tickers = request_data.get("tickers", [])
     date = request_data.get("date", "")
+    updateTickerList(tickers)
     db = getDBInstance()
     result = db.getDataInDateRange(tickers,date,date)
+    db.closeConnection()
     return Response(result)
 
 @api_view(["POST"])
@@ -56,12 +45,36 @@ def getDataForDateRange(request):
         request_data = json.loads(request.body.decode("utf-8"))
     except json.JSONDecodeError:
         return Response({"error": "Invalid JSON data"}, status=400)
-    """
-    Returns all of the data for the given tickers for the given day.
-    """
     tickers = request_data.get("tickers", [])
     aboveDate = request_data.get("aboveDate", "")
     belowDate = request_data.get("belowDate", "")
+    updateTickerList(tickers)
     db = getDBInstance()
     result = db.getDataInDateRange(tickers,aboveDate,belowDate)
+    db.closeConnection()
     return Response(result)
+
+# TODO: Turn this into a proper endpoint, and have the other views just send an HTTP request
+#@api_view(["POST"])
+def updateTickerList(tickers: List[str]):
+    db = getDBInstance()
+    api = getAlphaVantageAPIInstance()
+    cachedTicker = db.getMostRecentDateForTickers(tickers)
+    for ticker in tickers:
+        apiResponse = api.getTickerData(ticker)
+        dataToCache = apiResponse["Time Series (Daily)"]
+        dbFormOfData = []
+        if ticker not in cachedTicker:
+            print("{} not in db: cache from scratch.".format(ticker))
+            for date in dataToCache.keys():
+                dbFormOfData.append((ticker, date, dataToCache[date]["4. close"]))
+            db.insertData(dbFormOfData)
+        elif cachedTicker[ticker] != apiResponse["Meta Data"]["3. Last Refreshed"]:
+            for date in dataToCache.keys():
+                if date > cachedTicker[ticker]:
+                    dbFormOfData.append((ticker, date, dataToCache[date]["4. close"]))
+                else:
+                    break
+            pass
+    db.closeConnection()
+    return
