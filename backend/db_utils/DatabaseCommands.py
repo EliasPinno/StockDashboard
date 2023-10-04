5bccc90a9ea110287a304c969361b98dc130d533
EliasPinno <pucas946@gmail.com>
2023-10-03 21:49:32 -0600

    Completed static methods to connect, create table, drop table and disconnect.

--- backend/db_utils/DatabaseCommands.py
+++ backend/db_utils/DatabaseCommands.py
@@ -1,27 +1,28 @@
 import sqlite3
 from sqlite3 import Error
 
-DB_FILENAME = "PROJECT_DB"
-STOCK_TABLE_NAME = "stock_prices"
-
 class Database:
+    DB_FILENAME = "PROJECT_DB"
+    STOCK_TABLE_NAME = "stock_prices"
+    #IS_STORED_TABLE_NAME = "is_stored"
     conn: None | sqlite3.Connection = None
 
     @staticmethod
-    def getConnection():
+    def getConnection() -> sqlite3.Connection:
         """
         This method returns a database connection if one exists, or creates a new one
         if it does not already exist.
         """
         global conn
         if not conn: # We need to create connection
-            conn = sqlite3.connect(DB_FILENAME)
+            conn = sqlite3.connect(Database.DB_FILENAME)
         return conn
 
     @staticmethod
-    def closeConnection():
+    def closeConnection() -> None:
         """
         This method closes an existing database connection, setting the singleton to none.
+        This method does nothing if a connection does not exist.
         """
         global conn
         if conn: # connection exists, we need to close it
@@ -30,6 +31,28 @@ class Database:
     @staticmethod
     def createDatabase():
         """
-        This function creates the table(s) for the database if they do not already exist
+        This function creates the table(s) for the database if they do not already exist.
         """
-        conn = sqlite3.connect(DB_FILENAME)
+        conn = Database.getConnection()
+        cursor = conn.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS {} (
+            ticker TEXT CHECK(length(ticker) <= 5),
+            date DATE,
+            price FLOAT
+            PRIMARY KEY (ticker, date)
+        )
+        '''.format(Database.STOCK_TABLE_NAME))
+        # TODO: Think about putting in an is_stored table
+        cursor.close()
+
+    @staticmethod
+    def dropTable():
+        """
+        This function drops the table, wiping out the local cache if it exists
+        """
+        conn = Database.getConnection()
+        cursor = conn.cursor()
+        cursor.execute("DROP TABLE {}".format(Database.STOCK_TABLE_NAME))
+        print("Dropped table {}".format(Database.STOCK_TABLE_NAME))
+        cursor.close()
